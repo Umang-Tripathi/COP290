d5974d654310d5cbf7eed43042d345cf2d90f09e
Umang Tripathi <umangtripathi@Umangs-MacBook-Pro.local>
2024-04-07 02:06:47 +0530

    fixed points bug and coordinates of goal

--- game4/public/football.js
+++ game4/public/football.js
@@ -236,6 +236,7 @@ play_audio.addEventListener("click",()=>{
     start_noise.volume=1;
     run1.volume=1;
     run2.volume=1;
+    cheer.volume=1;
     
 
 })
@@ -248,6 +249,7 @@ stop_audio.addEventListener("click",()=>{
     start_noise.volume=0;
     run1.volume=0;
     run2.volume=0;
+    cheer.volume=0;
 })
 reset2.addEventListener("click",()=>{
     window.location.reload();
@@ -273,6 +275,7 @@ function update_match(){
     current_rotation=0
     document.getElementById("ballImg").style.rotate=(current_rotation)+"deg";
     start_noise.play();
+    ball.style.visibility="visible";
     timerplayer2=setInterval(move_player2,10);
     timerball=setInterval(move_ball,10);
     timerplayer1=setInterval(move_player1,10);
@@ -419,129 +422,74 @@ function move_ball(){
     let temp_ball_posn_y=ball_posn_y;
     ball_posn_x+=(ball_dir_x)*ball_speed;
     ball_posn_y+=(ball_dir_y)*ball_speed;
-    if(ball_posn_x<-5){
-        points1+=1;
-        ball_speed=0;
-        document.getElementById("points1").innerHTML=points1;
-        new_match();
-        colide.play();
-        //ball_posn_x=-5;
-        ball_dir_x=-ball_dir_x;
-    }
-    else if(ball_posn_x<0){
-        if(ball_posn_y<30){
+    if(ball_posn_x<=0){
+        if(ball_posn_y<25){
             ball_posn_x=0;
             ball_speed-=0.01;
             ball_dir_x=-ball_dir_x;
             colide.play();
         }
-        else if(ball_posn_y>50){
+        else if(ball_posn_y>55){
             ball_posn_x=0;
             ball_speed-=0.01;
             ball_dir_x=-ball_dir_x;
             colide.play();
         }
-        else if(ball_speed==0){
+        else{
             points1+=1;
             ball_speed=0;
             document.getElementById("points1").innerHTML=points1;
             ball_posn_x=39;
             ball_posn_y=38;
+            ball.style.visibility="hidden";
             new_match();
 
         }
         
     }
-    else if(ball_posn_x>86){
-        points2+=1;
-        ball_speed=0;
-        
-        document.getElementById("points2").innerHTML=points2;
-        new_match();
-        //ball_posn_x=86;
-        ball_dir_x=-ball_dir_x;
-        colide.play();
-    }
-    else if(ball_posn_x>76){
-        if(ball_posn_y<30){
+    else if(ball_posn_x>=76){
+        if(ball_posn_y<25){
+            ball_posn_x=76;
+            ball_speed-=0.01;
+            
+
+            ball_dir_x=-ball_dir_x;
+            colide.play();
+        }
+        else if(ball_posn_y>51){
             ball_posn_x=76;
             ball_speed-=0.01;
 
             ball_dir_x=-ball_dir_x;
             colide.play();
         }
-        else if(ball_posn_y>50){
-            ball_posn_x=76;
-            ball_speed-=0.01;
-
-            ball_dir_x=-ball_dir_x;
-            colide.play();
-        }
-        else if(ball_speed==0){
-            points1+=1;
+        else{
+            points2+=1;
             ball_speed=0;
             document.getElementById("points2").innerHTML=points2;
             new_match();
             ball_posn_x=39;
             ball_posn_y=38;
-
+            ball.style.visibility="hidden";
         }
         
     }
+    else{
+        if(ball_posn_y<=0){
+            ball_posn_y=0;
+            ball_speed-=0.01;
     
+            ball_dir_y=-ball_dir_y;
+            colide.play();
+        }
+        else if(ball_posn_y>=76){
+            ball_posn_y=76;
+            ball_speed-=0.01;
     
-
-
-
-    if(ball_posn_y<0){
-        ball_posn_y=0;
-        ball_speed-=0.01;
-
-        ball_dir_y=-ball_dir_y;
-        colide.play();
+            ball_dir_y=-ball_dir_y;
+            colide.play();
+        }
     }
-    else if(ball_posn_y<30){
-        if(ball_posn_x<0){
-            ball_posn_y=30;
-            ball_speed-=0.01;
-
-            ball_dir_y=-ball_dir_y;
-            colide.play();
-        }
-        if(ball_posn_x>76){
-            ball_posn_y=30;
-            ball_speed-=0.01;
-
-            ball_dir_y=-ball_dir_y;
-            colide.play();
-        }
-        
-        
-
-    }
-    else if(ball_posn_y>76){
-        ball_posn_y=76;
-        ball_speed-=0.01;
-
-        ball_dir_y=-ball_dir_y;
-        colide.play();
-    }
-    else if(ball_posn_y>50){
-        if(ball_posn_x<0){
-            ball_posn_y=50;
-            ball_speed-=0.01;
-
-            ball_dir_y=-ball_dir_y;
-            colide.play();
-        }
-        if(ball_posn_x>76){
-            ball_posn_y=50;
-            ball_speed-=0.01;
-
-            ball_dir_y=-ball_dir_y;
-            colide.play();
-        }
-    } 
     if(ball_posn_x!=temp_ball_posn_x || ball_posn_y!=temp_ball_posn_y){
         current_rotation+=10*ball_speed;
         
